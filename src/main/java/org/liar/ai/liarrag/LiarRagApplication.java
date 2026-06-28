618368ffa9969627d7fddcb9a871df7cb90704f3
peijiarui <545969190@qq.com>
2026-06-28 23:53:57 +0800

    project init

---
+++ src/main/java/org/liar/ai/liarrag/LiarRagApplication.java
@@ -0,0 +1,13 @@
+package org.liar.ai.liarrag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiarRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiarRagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/liar/ai/liarrag/LiarRagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.liar.ai.liarrag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiarRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
